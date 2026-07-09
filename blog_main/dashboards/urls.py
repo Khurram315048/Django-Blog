fb50af489bc9c274227002705ddeb693958e9ec2
Muhammad Khurram <saleemkhurram420@gmail.com>
2026-07-09 15:58:25 +0500

    Updating CRUD posts operation

--- blog_main/dashboards/urls.py
+++ blog_main/dashboards/urls.py
@@ -10,5 +10,6 @@ urlpatterns=[
     path('posts/',views.posts,name='posts'),
     path('posts/edit_post/<int:pk>/',views.edit_post,name='edit_post'),
     path('posts/add_post',views.add_post,name='add_post'),
-    path('posts/delete_post/<int:pk>/',views.delete_post,name='delete_post'),
+    path('posts/delete/<int:pk>/',views.delete_post,name='delete_post'),
+    
 ]
