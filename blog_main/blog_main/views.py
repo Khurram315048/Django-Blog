dd07dbbb47d5fb53a571767302d97d9c00cf8353
Muhammad Khurram <saleemkhurram420@gmail.com>
2026-07-09 12:55:08 +0500

    Updating the CRUD on Categories

--- blog_main/blog_main/views.py
+++ blog_main/blog_main/views.py
@@ -42,7 +42,7 @@ def login(request):
             if user is not None:
                 auth.login(request,user)
 
-            return redirect('home')    
+            return redirect('dashboard')    
 
     form=AuthenticationForm()
     context={
