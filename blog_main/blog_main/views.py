62f49dc6edbdbe3d2be473fceb3fa113880dc349
Muhammad Khurram <saleemkhurram420@gmail.com>
2026-07-09 10:49:50 +0500

    Add forms, login/register/logout views, and templates

--- blog_main/blog_main/views.py
+++ blog_main/blog_main/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blogs.models import Category,Blog
+from .forms import RegisterationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     
@@ -12,3 +15,42 @@ def home(request):
         'posts':posts,
     }
     return render(request,'home.htm',context)
+
+
+def register(request):
+    if request.method=='POST':
+        form=RegisterationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:    
+        form=RegisterationForm()
+
+    context={
+        'form':form,
+    }
+    return render(request,'register.htm',context)
+
+
+def login(request):
+    if request.method=='POST':
+        form=AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+
+            return redirect('home')    
+
+    form=AuthenticationForm()
+    context={
+        'form':form,
+    }
+    return render(request,'login.htm',context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
